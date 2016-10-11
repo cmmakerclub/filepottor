0ebc92d5b4cb2918a20cd3e2c97651f137a86717
apinuntong <apinun.tong@gmail.com>
2016-10-11 16:13:13 +0700

    Add files via upload
    ให้แมน

---
+++ v1/v1.ino
@@ -0,0 +1,37 @@
+int x = 0;
+void setup() {
+  pinMode(A3, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(0, y, RISING);
+  pinMode(1, OUTPUT);
+   pinMode(A2, OUTPUT);
+}
+
+void loop() {
+  //t();
+   digitalWrite(A2, LOW);
+}
+void t(){
+    if(x>0){
+    digitalWrite(1, LOW);
+  }
+  
+  if(x<0){
+    digitalWrite(1, HIGH);
+  }
+    if(x==0){
+    digitalWrite(1, LOW);
+    
+  }
+}
+void y(){
+   digitalWrite(A2, HIGH);
+  if(digitalRead(A3)==1){
+    x = x - 1;
+  }
+  if(digitalRead(A3)==0){
+    x = x + 1;
+  }
+}
+
+
